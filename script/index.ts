26ebca5d2c48d6eff8da6ccb23735c48d835a7da
Pavel Kryuchkov <kareglas83@gmail.com>
2021-07-16 09:59:33 +0300

    Add catch Exception and fix settings.

--- script/index.ts
+++ script/index.ts
@@ -10,12 +10,20 @@ const rl = createInterface({
 const question = (): Promise<null> =>
   new Promise((resolve) => {
     rl.question("Введите выражение: ", (answer: string) => {
-      const result = runner(answer);
 
-      if (result !== undefined) {
-        // eslint-disable-next-line no-console
-        console.log(`Результат: ${result}`);
-      }
+        if (!answer) {
+            console.log('Введите данные!')
+        } else {
+            try {
+                const result = runner(answer);
+                if (result !== undefined) {
+                    // eslint-disable-next-line no-console
+                    console.log(`Результат: ${result}`);
+                }
+            } catch (error) {
+                console.log('Ошибка: ' + error)
+            }
+        }
 
       resolve(null);
     });
